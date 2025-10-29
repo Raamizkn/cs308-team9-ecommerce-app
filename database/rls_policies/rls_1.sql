3024ebce4a7aec0d73c07641df59e1a9d829cb79
rasulbarak <rasul.barak@sabanciuniv.edu>
2025-10-29 13:57:53 +0300

    add rls policy for discount mappings and campaigns

--- database/rls_policies/rls_1.sql
+++ database/rls_policies/rls_1.sql
@@ -1,9 +1,13 @@
+-- CATEGORIES
+
 ALTER TABLE public.categories ENABLE ROW LEVEL SECURITY;
 -- Anyone should be able to see the categoris
 CREATE POLICY "Allow public read access to categories"
     ON public.categories FOR SELECT
     USING (true);
 
+
+-- PRODUCTS_BELONG_TO
 
 ALTER TABLE public.products_belong_to ENABLE ROW LEVEL SECURITY;
 -- Anyone should be able to see the products
@@ -12,3 +16,19 @@ CREATE POLICY "Allow public read access to products"
     using (true);
 
 
+-- DISCOUNT_CAMPAIGNS
+
+ALTER TABLE public.discount_campaigns ENABLE ROW LEVEL SECURITY
+-- Anyone should be able to access the discount campaigns in order to calcualte discounted price
+CREATE POLICY "Allow public read access to discount campaigns"
+    ON public.discount_campaigns FOR SELECT
+    USING (true);
+
+
+-- HAS_DISCOUNT
+
+ALTER TABLE public.has_discount ENABLE ROW LEVEL SECURITY
+-- Anyone should be able to see if a product has a discount campaign
+CREATE POLICY "Allow public read access to discount mappings of products"
+    ON pubic.discount_campaigns FOR SELECT
+    USING (true);
